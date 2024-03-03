101afe38d17fb725841866679ef31c21ed978dd9
Alexander Malyatin <paprikka84@gmail.com>
2024-03-03 16:47:01 +0600

    Fixed README

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2701472dd7647074698e3fe3c4bcebab09b913")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a40e02279512e6c06dada65200f98ae7d72a092")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
