14957bca8e6223c61c956c640f855bea26827ba3
Alexander Malyatin <paprikka84@gmail.com>
2024-03-04 13:40:01 +0600

    Add HW_Lesson6:Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+using System.Text;
+
+bool CheckPalindrom(string stringToCheck)
+{
+  bool result = new bool();
+  string lowerString = stringToCheck.ToLower();
+  for (int i = 0; i < lowerString.Length / 2; i++)
+  {
+    if (lowerString[i] != lowerString[lowerString.Length - 1 - i])
+    {
+      result = false;
+      break;
+    }
+    else
+    { result = true; }
+  }
+  return result;
+}
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Console.Write("Введите строку: ");
+string inputString = Console.ReadLine();
+if (CheckPalindrom(inputString))
+  Console.WriteLine($"Строка {inputString} - палиндром.");
+else
+  Console.WriteLine($"Строка {inputString} - не палиндром.");
